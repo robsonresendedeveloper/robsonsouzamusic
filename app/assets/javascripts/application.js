d38976fbf7ee95e4f6791bc0b3b7f7239b96a9a3
Robson Resende <robson.souza.cl@gmail.com>
2019-02-19 15:25:44 -0300

    Fixing background bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,33 +18,6 @@
 //= require_tree .
 
 $("document").ready(function(){
-	$(".banner2").hide();
-  $(".banner3").hide();
-
-	var img=1;
-	setInterval(function(){
-		switch(img) {
-			case 1:
-				img=2;
-				$(".banner1").fadeOut(1600, function(){
-          $(".banner2").fadeIn(1600);
-				});
-			break;
-			case 2:
-				img=3;
-				$(".banner2").fadeOut(1600, function(){
-					$(".banner3").fadeIn(1600);
-				});
-			break;
-			case 3:
-				img=1;
-				$(".banner3").fadeOut(1600, function(){
-					$(".banner1").fadeIn(1600);
-				});
-			break;
-		}
-	}, 5000);
-
 
 	$("#title-message").hide();
 
